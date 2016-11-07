6a235b6180f88c4b6e8ee0d188d8adc8e1bd6960
juan cruz <juancruzvalera@hotmail.com>
2016-11-07 08:55:59 -0300

    first

---
+++ app/src/main/java/com/ejemplo/album/view/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ejemplo.album.view;
+
+import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.ejemplo.album.R;
+
+import java.util.List;
+
+import util.ResultListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
